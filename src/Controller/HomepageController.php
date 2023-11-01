04f20c0cf87801c5f04af526e21b893dd4f35a2c
Yvan Emiliau <yvanemiliau@gmail.com>
2023-11-01 09:38:55 +0300

    homepage route

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HomepageController {
+
+	public function index()
+	{
+		return new Response("This is my homepage");
+	}
+}
+
+?>
