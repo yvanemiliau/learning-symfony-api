891f9c343ec7c753d68d3a6cb4f88b4331d85af4
Yvan Emiliau <yvanemiliau@gmail.com>
2023-11-01 09:53:22 +0300

    use annotations instead of routes.yaml (require doctrine/annotations)

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController {
 
+	/**
+	 * @Route("/", name="homepage")
+	 */
 	public function index()
 	{
 		return new Response("This is my homepage");
